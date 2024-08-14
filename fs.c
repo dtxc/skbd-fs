7aa061be306877be4aed0016c942c3789dd3a4ce
nick <69575053+dtxc@users.noreply.github.com>
2024-08-14 12:43:24 +0300

    Implement fopen() and separate find_node()

--- fs.c
+++ fs.c
@@ -35,6 +35,10 @@ char **split_string(const char *str, const char *delimiter, int *num_tokens) {
     return result;
 }
 
+uint8_t mode_calc(uint8_t user, uint8_t kernel) {
+    return user << 3 | kernel;
+}
+
 void new_fs(FILE *fp, uint64_t size, int isos) {
     if (size % BLOCK_SIZE != 0) {
         printf("fs size must be a multiple of %d kib", BLOCK_SIZE / 1024);
@@ -62,7 +66,7 @@ void new_fs(FILE *fp, uint64_t size, int isos) {
     node->flags |= FS_DIR;
     node->magic = NODE_MAGIC;
     node->size = 0;
-    node->mode = 7;
+    node->mode = mode_calc(MODE_R, MODE_R | MODE_W);
     strcpy(node->name, "ROOT");
 
     memcpy(blk->data, node, sizeof(node_t));
@@ -88,7 +92,7 @@ void new_fs(FILE *fp, uint64_t size, int isos) {
 
     iblk: starting block
 */
-uint32_t find_next_free_block(FILE *fp, block_t *dest, uint32_t iblk) {
+static uint32_t find_next_free_block(FILE *fp, block_t *dest, uint32_t iblk) {
     uint64_t offset = iblk * BLOCK_SIZE;
     uint32_t fs_size = 1048576;
 
@@ -112,7 +116,7 @@ uint32_t find_next_free_block(FILE *fp, block_t *dest, uint32_t iblk) {
     return 0;
 }
 
-block_t *get_block_by_index(FILE *fp, uint32_t iblk) {
+static block_t *get_block_by_index(FILE *fp, uint32_t iblk) {
     uint64_t offset = iblk * BLOCK_SIZE;
     block_t *blk = malloc(BLOCK_SIZE);
 
@@ -122,9 +126,9 @@ block_t *get_block_by_index(FILE *fp, uint32_t iblk) {
     return blk;
 }
 
-int mknode(FILE *fp, char *pwd, char *name, int type) {
+static uint32_t find_node(FILE *fp, char *path, int type, node_t *dst) {
     int nsplits = 0;
-    char **pwd_split = split_string(pwd, "/", &nsplits);
+    char **pwd_split = split_string(path, "/", &nsplits);
     block_t *blk = get_block_by_index(fp, 1);
     node_t *node = malloc(sizeof(node_t));
 
@@ -139,21 +143,23 @@ int mknode(FILE *fp, char *pwd, char *name, int type) {
         found = 0;
         for (int j = 0; j < node->size; j++) { // iterate through each subdirectory of the parent.
             // possible data corruption
-            if (*children == 0) return 1;
+            if (*children == 0) return 0;
 
             b = get_block_by_index(fp, *children);
             assert(b->attributes & BLOCK_METADATA); // block has to contain a node
             memcpy(temp, b->data, sizeof(node_t));
             assert(temp->magic == NODE_MAGIC);
 
-            if (temp->flags & FS_DIR) {
+            int target_type = (i == nsplits-1) ? type : FS_DIR;
+            if (temp->flags & target_type) {
                 if (strcmp(temp->name, pwd_split[i]) == 0) {
                     found = 1;
+                    block_offset = *children;
                     memcpy(node, temp, sizeof(node_t));
                     memcpy(blk, b, BLOCK_SIZE);
-                    block_offset = *children;
 
                     children = (uint32_t *) (blk->data + sizeof(node_t));
+                    free(b);
                     break;
                 }
             }
@@ -162,10 +168,29 @@ int mknode(FILE *fp, char *pwd, char *name, int type) {
             free(b);
         }
 
-        // directory was not found.
-        if (found == 0) return -1;
+        // node was not found.
+        if (found == 0) {
+            return 0;
+        }
     }
+    memcpy(dst, node, sizeof(node_t));
+    free(node);
     free(temp);
+
+    return block_offset;
+}
+
+int mknode(FILE *fp, char *pwd, char *name, int type) {
+    node_t *node = malloc(sizeof(node_t));
+    uint32_t block_offset = find_node(fp, pwd, FS_DIR, node);
+    if (block_offset == 0) {
+        free(node);
+        printf("pwd: %s, name: %s\n", pwd, name);
+        return -1;
+    }
+
+    block_t *blk = get_block_by_index(fp, block_offset);
+    memcpy(node, blk->data, sizeof(node_t));
 
     uint32_t parent_size = node->size;
     uint32_t parent_off = node->first_block;
@@ -188,7 +213,9 @@ int mknode(FILE *fp, char *pwd, char *name, int type) {
     node->magic = NODE_MAGIC;
     node->first_block = r;
     node->flags |= type;
-    node->mode = (type == FS_FILE) ? 7 : (MODE_R | MODE_W);
+    if (type == FS_FILE) node->mode = 63; // rwx rwx
+    else if (type == FS_DIR) node->mode = 54; // rw- rw-
+    else if (type == FS_CHARDEV) node->mode = 38; // r-- rw-
     node->size = 0;
     strcpy(node->name, name);
 
@@ -200,4 +227,77 @@ int mknode(FILE *fp, char *pwd, char *name, int type) {
     free(blk);
     free(node);
     return 0;
+}
+
+// if both append and write is selected, write will be executed
+file_t *_fopen(FILE *fp, char *path, uint8_t mode) {
+    if (mode == 0) return NULL;
+
+    node_t *node = malloc(sizeof(node_t));
+    uint32_t off = find_node(fp, path, FS_FILE, node);
+
+    if (off == 0) {
+        free(node);
+        return NULL;
+    }
+
+    file_t *file = malloc(sizeof(file_t));
+
+    if (mode & FMODE_W) {
+        if ((node->mode & MODE_W) == 0) {
+            free(node);
+            free(file);
+            return NULL; // not writeable
+        }
+
+        file->mode = node->mode;
+        file->open_mode = mode;
+        file->ptr_local = 0;
+        file->ptr_global = BLOCK_SIZE * node->first_block;
+        file->size = node->size;
+        file->start = node->first_block;
+        strcpy(file->name, node->name);
+    } else if (mode & FMODE_A) { // append
+        if ((node->mode & MODE_W) == 0) {
+            free(node);
+            free(file);
+            return NULL;
+        }
+
+        // we need to find the last block to set the file pointer
+        block_t *block = get_block_by_index(fp, node->first_block);
+        int next;
+        while (block->next != 0) {
+            next = block->next;
+            free(block);
+
+            block = get_block_by_index(fp, block->next);
+        }
+
+        file_t *file = malloc(sizeof(file_t));
+        file->mode = node->mode;
+        file->open_mode = mode;
+        file->ptr_local = node->size;
+        file->ptr_global = next * BLOCK_SIZE + (BLOCK_SIZE - node->size % BLOCK_SIZE);
+        file->size = node->size;
+        file->start = node->first_block;
+        strcpy(file->name, node->name);
+    } else if (mode & FMODE_R) {
+        file_t *file = malloc(sizeof(file_t));
+        file->mode = node->mode;
+        file->open_mode = mode;
+        file->ptr_local = 0;
+        file->ptr_global = BLOCK_SIZE * node->first_block;
+        file->size = node->size;
+        file->start = node->first_block;
+        strcpy(file->name, node->name);
+    }
+
+    free(node);
+    return file;
+}
+
+int main() {
+    FILE *fp = fopen("image.bin", "r+b");
+    new_fs(fp, 1024 * 1024, 1);
 }
